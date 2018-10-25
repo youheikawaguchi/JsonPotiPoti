02f9410753c7498ad7495e95b15440271883e3fd
youheikawaguchi <plmnbhuiokj@gmail.com>
2018-10-25 10:21:56 +0900

    送信完了

---
+++ app/src/main/java/com/example/g015c1153/jsonpotipoti/LoginData.kt
@@ -0,0 +1,6 @@
+package com.example.g015c1153.jsonpotipoti
+
+data class LoginData(
+    val id : String,
+    val pw: String
+)
--- app/src/main/java/com/example/g015c1153/jsonpotipoti/MainActivity.kt
+++ app/src/main/java/com/example/g015c1153/jsonpotipoti/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.g015c1153.jsonpotipoti
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
@@ -15,15 +17,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val url = "https://www.google.co.jp/"
+        val url = "https://toridge.com/post_json.php"
         val handler = Handler()
-        val request = Request.Builder()
-            .addHeader("Content-Type", "text/plain; charset=utf-8")
-            .url(url)
-            .build()
-
 
         JsonButton.setOnClickListener {
+            val request = Request.Builder()
+                .addHeader("Content-Type", "text/plain; charset=utf-8")
+                .url(url)
+                .build()
+            OkHttpClient().newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {}
+                override fun onResponse(call: Call, response: Response) {
+                    val responseText: String? = response.body()?.string()
+                    handler.post {
+                        JsonText.text = responseText
+                    }
+                }
+            })
+        }
+
+        JsonPush.setOnClickListener {
+
+            val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()!!
+            val loginAdapter = moshi.adapter(LoginData::class.java)!!
+            val id :String = jsonId.text.toString()
+            val pw :String = jsonPw.text.toString()
+            val loginData = LoginData(id,pw)
+            val loginJson = loginAdapter.toJson(loginData)
+            val postBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), loginJson)
+            val request = Request.Builder()
+                .addHeader("Content-Type", "text/plain; charset=utf-8")
+                .url(url)
+                .post(postBody)
+                .build()
+
             OkHttpClient().newCall(request).enqueue(object : Callback {
                 override fun onFailure(call: Call, e: IOException) {}
                 override fun onResponse(call: Call, response: Response) {
